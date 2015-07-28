6e473923591cea5340bd6438e4a81c74f38dfe05
Scott Benes <sbenes@vipasolutions.com>
2015-07-28 11:56:46 -0500

    Add icon font generation files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
-var watch = require('gulp-watch');
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+var rename = require('gulp-rename');
+var _ = require('lodash');
 
 
 gulp.task('default', ['styles']);
@@ -23,6 +26,31 @@ gulp.task('styles:build-scss', ['styles:copy-css'], function() {
 			cascade: false
 		}))
 		.pipe(gulp.dest('./web/stylesheets/build'));
+});
+
+gulp.task('iconfont', function(){
+	return gulp.src('./font-glyphs/src/icons/*.svg')
+		.pipe(iconfont({
+			fontName: 'vp-glyph-font', // required
+			appendCodepoints: true, // recommended option
+			startCodepoint: 0xF101,
+			fontHeight: 150
+		}))
+		.on('codepoints', function(codepoints, options) {
+			codepoints = _.map(codepoints, function(codepoint) {
+				return {
+					name: codepoint.name,
+					codepoint: codepoint.codepoint.toString(16).toLowerCase()
+				};
+			});
+			gulp.src('./font-glyphs/src/templates/_iconfont.scsstpl')
+				.pipe(consolidate('lodash', {
+					glyphs: codepoints
+				}))
+				.pipe(rename('_font-glyph-entities.scss'))
+				.pipe(gulp.dest('./web/stylesheets/src/config/'));
+		})
+		.pipe(gulp.dest('./web/design/fonts/glyph-lib/'));
 });
 
 gulp.task('styles', ['styles:build-scss']);
